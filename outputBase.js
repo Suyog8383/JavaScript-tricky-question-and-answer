3cd18c6bcb13505521e509d21a60e22a581e6e43
Suyog Kalyan Nagawade <92072200+Suyog8383@users.noreply.github.com>
2023-07-11 22:20:06 +0530

    outputBase Que of JavaScript

---
+++ outputBase.js
@@ -0,0 +1,6 @@
+console.log(typeof NaN);    //number
+console.log(2 === [2]);     //false
+console.log(1 < 2 < 3);     //true
+console.log([] + []);       //""
+console.log(2 + '2' + 1);   //"221"
+console.log(2 + '2' - 1);   //21
